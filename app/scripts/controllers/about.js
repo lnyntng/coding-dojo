da47198d1117ac9d6e5b0b429834637446851352
Katy Lion <lnyntng@gmail.com>
2017-10-15 19:46:31 -0600

    First commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name codingDojoApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the codingDojoApp
+ */
+angular.module('codingDojoApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name codingDojoApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the codingDojoApp
+ */
+angular.module('codingDojoApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
