4bf378fb910887af9db048ca91da89ff5e123212
Katy Lion <lnyntng@gmail.com>
2017-10-15 20:37:21 -0600

    Delete unnecesary views

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,9 +9,5 @@
  */
 angular.module('codingDojoApp')
   .controller('MainCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+    this.calculateFizzBuzz = 1;
   });
