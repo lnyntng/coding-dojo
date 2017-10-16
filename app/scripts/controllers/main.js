9a7c1ded2e3cb573aa98e0636786ff028707eb46
Katy Lion <lnyntng@gmail.com>
2017-10-15 20:59:23 -0600

    Added test and implementation for numbers divisible by 5

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -11,7 +11,18 @@ angular.module('codingDojoApp')
   .controller('MainCtrl', function () {
   	this.value = 1;
 
-    this.calculateFizzBuzz = function(value){
-    	return 1;
+    this.calculateFizzBuzz = function(value) {
+    	var arr = '1';
+    	for (var i = 2; i <= value; i++) {
+    		if(i % 3 === 0) {
+    			arr += ',Fizz';
+    		} else if(i % 5 === 0) {
+    			arr += ',Buzz';
+    		} else {
+    			arr += ',' + i;
+    		}
+    	}
+    	console.log(arr);
+    	return arr;
     };
   });
--- test/spec/controllers/main.js
+++ test/spec/controllers/main.js
@@ -19,7 +19,15 @@ describe('Controller: MainCtrl', function () {
     }));
 
     it('should equal 1 when param value is 1', function () {
-      expect(MainCtrl.calculateFizzBuzz(1)).toEqual(1);
+      expect(MainCtrl.calculateFizzBuzz(1)).toEqual('1');
+    });
+
+    it('should equal 1,2,Fizz when param value is 3', function () {
+      expect(MainCtrl.calculateFizzBuzz(3)).toEqual('1,2,Fizz');
+    });
+
+    it('should equal 1,2,Fizz,4,Buzz when param value is 5', function () {
+      expect(MainCtrl.calculateFizzBuzz(5)).toEqual('1,2,Fizz,4,Buzz');
     });
   });
 });
